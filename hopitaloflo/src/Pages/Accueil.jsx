d5f55b0af6ceaa305eb70b60a9f8ccb023cb558d
Poteau Florian <florianpoteau59@gmail.com>
2024-04-22 15:40:00 +0200

    feat(Router): ajout du router dans App.js

---
+++ hopitaloflo/src/Pages/Accueil.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Accueil = () => {
+    return (
+        <div className='col-sm-6'>
+            <h1>Hello</h1>
+        </div>
+    );
+};
+
+export default Accueil;
