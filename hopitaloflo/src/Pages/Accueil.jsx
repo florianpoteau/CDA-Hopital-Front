2260c24f20a3b28f4bc0483103a3d455397f0421
Poteau Florian <florianpoteau59@gmail.com>
2024-04-29 09:19:57 +0200

    fix(dropdown services): ajout de l'index dans chaque option

--- hopitaloflo/src/Pages/Accueil.jsx
+++ hopitaloflo/src/Pages/Accueil.jsx
@@ -17,8 +17,8 @@ const Accueil = () => {
             <div className='col-md-2 col-6 mb-5 ms-5 ps-md-5 ms-md-5 '>
                 <Form.Select size="md" className='mt-5'>
                     <option>Rechercher par service</option>
-                    {services.map((service) => {
-                        return <option>{service.name}</option>
+                    {services.map((service, index) => {
+                        return <option key={index}>{service.name}</option>
                     })}
                 </Form.Select>   
             </div>
